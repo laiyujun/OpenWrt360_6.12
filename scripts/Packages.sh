631cd03f856d90d634b1488767289be8f0d6dc15
laiyj <laiyujun@vip.qq.com>
2026-08-10 12:43:17 +0800

    ad luci-theme-aurora

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -45,8 +45,8 @@ rm -rf feeds/packages/lang/golang
 #UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "argon-config" "jerrykuku/luci-app-argon-config" "master"
-UPDATE_PACKAGE "alpha" "derisamedia/luci-theme-alpha" "master"
-UPDATE_PACKAGE "alpha-config" "animegasan/luci-app-alpha-config" "master"
+UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
+UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 
 #UPDATE_PACKAGE "homeproxy" "VIKINGYFY/homeproxy" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "master" "pkg"
